28dd5cfd0fc5f034ca6ed9d677865db0dbda83e8
Keita Yamazaki <recoder.coding@gmail.com>
2017-03-10 16:05:25 +0900

    1.2節のアルゴリズム「線形探索」
    小さい順に並べられた長さnの整数の配列Aに，与えられたbという数が含まれているかを判定する．
    計算時間の合計は，最悪の場合ループをn回まわるので，ループ内の計算時間をaとすると，全体でanとなり，
    定数部分を除けば，nとなり計算量はO(n)とかける．

---
+++ linearSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            List<int> A = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                A.Add(a);
+            }
+
+            bool ans = new Program().linearSearch(n, A, b);
+            if (ans)
+                Console.WriteLine("見つかったよ");
+            else
+                Console.WriteLine("見つからなかったよ");
+        }
+
+        public bool linearSearch(int n, List<int> A, int b)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == b)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
